b2a3d528f0250e2843f174dc26f4ce2d8cac4464
catsget <120100772+catsget@users.noreply.github.com>
2025-02-01 18:43:52 +0300

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const PostSchema = new mongoose.Schema({
+    author: String,
     title: String,
     content: String,
     createdAt: { type: Date, default: Date.now }
